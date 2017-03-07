33dc2b90e81100818b8ca1c9659b66291483c412
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-03-08 02:40:20 +0300

    Переходим на работу с классами

--- progs/reglament_monitor.py
+++ progs/reglament_monitor.py
@@ -5,29 +5,35 @@ from sys      import path
 
 path.append ('/data/git/Python/moduls')
 
-from reglament_classes import StaffType, Host, StaffNeed
+from reglament_classes import StaffType, Host, StaffNeed, StaffPeriod, StaffLog
 from reglament_defs    import write_db, update_db, read_db
 
 NOW = datetime.today ()
+#print (NOW)
+#input ()
 
-for id_host in read_db (tables = 'hosts', columns = 'id', case = 'id > 0', type = 'all'):
-  host = Host (id_host [0])
-
-  for id_staff_type in read_db (tables = 'staff_types', case = 'id > 0', columns = 'id', type = 'all'):
-    staff_type = StaffType (id_staff_type [0])
-
-    staff_need = read_db (tables = 'staff_need', columns = 'id', case = 'host = %d AND type = %d' % (host.id, staff_type.id), type = 'val')
-    if not staff_need: write_db (table = 'staff_need', columns = ('host', 'type', 'is_need'), row = (host.id, staff_type.id, 0))
-    else:             update_db (table = 'staff_need', column = 'is_need', value = 0, case = 'host = %d AND type = %d' % (host.id, staff_type.id))
-
-    period     = host.staff_periods [staff_type.name]
-    last       = host.staff_lasts   [staff_type.name]
-
-    if not period:
-      continue
-
+for host in Host.all ():
+  for staff_type in StaffType.all ():
+    staff_period = StaffPeriod (host = host, type = staff_type)
+    if not staff_period.is_find (): continue
+    log = StaffLog ()
+    last = log.last (host = host, type = staff_type)
     if not last: last = date (1, 1, 1)
-    control_date = datetime.combine ((last + timedelta (days = period)), time (0, 0, 0))
-    if NOW > control_date:
-      update_db (table = 'staff_need', column = 'is_need', value = 1, case = 'host = %d AND type = %d' % (host.id, staff_type.id))
+    control_date = datetime.combine (last, time (0, 0, 0)) + timedelta (days = staff_period.period)
+    staff_need = StaffNeed (host = host, type = staff_type)
+    staff_need.last_control = NOW
+    staff_need.is_need = False
+    if NOW >= control_date:
+      staff_need.is_need = True
+    staff_need.write ()
+#    print (
+#      '''
+#Host:         %s
+#Staff:        %s
+#Period:       %d
+#Last:         %s
+#Control date: %s
+#Is need:      %s
+#      ''' % (host.name, staff_type.name, staff_period.period, last, control_date, staff_need.is_need)
+#    )
 
