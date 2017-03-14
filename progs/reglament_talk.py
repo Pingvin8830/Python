bc0b416ba45b4a0348b08888942e11704761c14f
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-03-14 19:04:29 +0300

    Контроль всех хостов

--- progs/reglament_talk.py
+++ progs/reglament_talk.py
@@ -1,15 +1,12 @@
 #!/bin/python
 
-from os   import uname, system as bash
+from os   import system as bash
 from sys  import path
 from time import sleep
 
 path.append ('/data/git/Python/moduls')
 
 from reglament_classes import Host, StaffType, StaffNeed
-
-THIS_HOST = uname ().nodename
-host = Host (name = THIS_HOST)
 
 def not_ping_server ():
   error = bool (bash ('ping -c 1 HomeServer 1>&- 2>&-'))
@@ -18,23 +15,23 @@ def not_ping_server ():
 
 def is_notify ():
   doing = False
-  try:
-    list_staff_needs = StaffNeed.filter (host = host)
-    if not list_staff_needs: doing = True
-    for staff_need in list_staff_needs:
-      if staff_need.is_need:
-        doing = True
-        bash ('notify-send "Требуется %s"' % staff_need.type.comment)
-  except:
-    doing = True
-    bash ('notify-send "Ошибка получения данных с сервера"')
+  for host in Host.all ():
+    try:
+      list_staff_needs = StaffNeed.filter (host = host)
+      if not list_staff_needs: doing = True
+      for staff_need in list_staff_needs:
+        if staff_need.is_need:
+          doing = True
+          bash ('notify-send "%s требуется %s"' % (host.name, staff_need.type.comment))
+    except:
+      doing = True
+      bash ('notify-send "Ошибка получения данных с сервера"')
   return doing
 
 while not_ping_server ():
   sleep (10)
   
 while is_notify ():
-  host = Host (name = THIS_HOST)
   sleep (10)
 
 bash ('notify-send "Обслуживание не требуется"')
