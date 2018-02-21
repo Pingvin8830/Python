8ddf52e74ab2f543dd8c70857068d88f71d5398a
Vlasov Anton <Vlasov-Ant@yandex.ru>
2018-02-21 16:41:46 +0300

    Создание моделей

--- books/sample/page/models.py
+++ books/sample/page/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length = 30, unique = True)
+#  description = models.TextField()
+
+class Good(models.Model):
+  name = models.CharField(max_length = 50, unique = True, verbose_name = "Название")
+  description = models.TextField()
+  in_stock = models.BooleanField(default = True, db_index = True, verbose_name = "В наличии")
+  category = models.ForeignKey(Category, null = True, blank = True, on_delete = models.SET_NULL)
+
+class BlogArticle(models.Model):
+  title = models.CharField(unique_for_date = "pubdate")
+  pubdate = models.DateField(auto_now_add = True)
+  updated = models.DateField(auto_now = True)
