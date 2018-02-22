8535174547c64950947504ee7fa7763c689d9e9e
Vlasov Anton <Vlasov-Ant@yandex.ru>
2018-02-22 14:27:39 +0300

    Генерация исключения Http404 в представлении index

--- books/django_practic_create_web-apps/sample/page/views.py
+++ books/django_practic_create_web-apps/sample/page/views.py
@@ -8,7 +8,10 @@ def index(request, cat_id):
   if cat_id == None:
     cat = Category.objects.first()
   else:
-    cat = Category.objects.get(pk = cat_id)
+    try:
+      cat = Category.objects.get(pk = cat_id)
+    except Category.DoesNotExist:
+      raise Http404
   goods = Good.objects.filter(category = cat).order_by ("name")
   s = "Категория: " + cat.name + "<br><br>"
   for good in goods:
