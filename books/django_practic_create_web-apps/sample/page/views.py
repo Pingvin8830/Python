15522207cdc33551f609c3aeb431e35c91e74b80
Vlasov Anton <Vlasov-Ant@yandex.ru>
2018-02-22 14:16:18 +0300

    Создание примитивного представления index

--- books/django_practic_create_web-apps/sample/page/views.py
+++ books/django_practic_create_web-apps/sample/page/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from page.models import Category, Good
 
 # Create your views here.
+
+def index(request, cat_id):
+  if cat_id == None:
+    cat = Category.objects.first()
+  else:
+    cat = Category.objects.get(pk = cat_id)
+  goods = Good.objects.filter(category = cat).order_by ("name")
+  s = "Категория: " + cat.name + "<br><br>"
+  for good in goods:
+    s = s + "(" + str(good.pk) + ") " + good.name + "<br>"
+  return HttpResponse(s)
+
