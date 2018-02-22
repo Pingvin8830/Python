56f77325a4106d5b5c04175f8b0b371c3e5f09a9
Vlasov Anton <Vlasov-Ant@yandex.ru>
2018-02-22 14:19:09 +0300

    Исправление ошибок

--- books/django_practic_create_web-apps/sample/page/urls.py
+++ books/django_practic_create_web-apps/sample/page/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from page import views
 
 urlpatterns = [
-  url(r'^(?:(?P<id>\d+)/)?$', views.index, name = "index"),
+  url(r'^(?:(?P<cat_id>\d+)/)?$', views.index, name = "index"),
   url(r'^good/(?P<id>\d+)/$', views.good, name = "good"),
 ]
 
